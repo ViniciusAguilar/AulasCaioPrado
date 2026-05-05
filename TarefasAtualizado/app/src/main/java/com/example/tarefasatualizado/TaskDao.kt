75ee45cbf150a7b2c96c0df519a542e2f530bc16
ViniciusAguilar <69908425+ViniciusAguilar@users.noreply.github.com>
2026-05-05 15:42:35 -0300

    App Tarefas atualizado
    App de tarefas atualizado

---
+++ TarefasAtualizado/app/src/main/java/com/example/tarefasatualizado/TaskDao.kt
@@ -0,0 +1,19 @@
+package com.example.tarefasatualizado
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM tasks")
+    fun getAllTasks(): List<Task>
+    @Insert
+    fun insetTask(task: Task)
+    @Update
+    fun updateTask(task: Task)
+    @Delete
+    fun deleteTask(task: Task)
+}
